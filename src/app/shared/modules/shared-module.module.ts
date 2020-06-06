372b265ef821c63e4ab0c9b14d4db1ac83d3af38
Yura Kochut <kochutyurawork@gmail.com>
2020-06-06 03:52:46 +0300

    add (modules)

--- src/app/shared/modules/shared-module.module.ts
+++ src/app/shared/modules/shared-module.module.ts
@@ -2,11 +2,24 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from "@angular/material/form-field";
 import { AngularMaterialModule } from "./angular-material/angular-material.module";
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, AngularMaterialModule],
-  exports: [AngularMaterialModule],
+  imports: [
+    CommonModule,
+    AngularMaterialModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
+  exports: [
+    AngularMaterialModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
   providers: [
     {
       provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
