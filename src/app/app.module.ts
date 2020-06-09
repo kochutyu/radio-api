08205f8c6ab7153bf93613aaa95c0e39c732a54e
Yura Kochut <kochutyurawork@gmail.com>
2020-06-09 16:22:51 +0300

    update module ( shared | change strategy )

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { SharedModule } from "./shared/modules/shared-module.module";
+import { SharedModule } from "./shared/shared-module.module";
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/shared/shared-module.module.ts
+++ src/app/shared/shared-module.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from "@angular/material/form-field";
-import { AngularMaterialModule } from "./angular-material/angular-material.module";
+import { AngularMaterialModule } from "./modules/angular-material/angular-material.module";
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
